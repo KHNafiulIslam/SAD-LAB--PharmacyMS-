3df0e5be0b7ffc24ac5d26293e4ee753ebef9cfa
KHNafiulIslam <nafiulovi63@gmail@gmail.com>
2020-12-15 21:11:32 +0600

    first commit

---
+++ gellary.php
@@ -0,0 +1,189 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/index.css">
+    <title>Doctors Gellary</title>
+  </head>
+  <body>
+      
+  <div class="site-wrap">
+    <header class="site-navbar">
+      <div class="site-navbar-top">
+        <div class="container">
+          <div class="row align-items-center">
+
+            <div class="col-6 col-md-4 order-2 order-md-1">
+<!--
+              <form action="shop.php" class="site-block-top-search">
+                <input class="form-control me-2" type="search" name="query" placeholder="Search" aria-label="Search" value="">
+              </form>
+-->
+            </div>
+
+            <div class="col-12 mb-3 mb-md-0 col-md-4 order-1 order-md-2 text-center">
+              <div class="site-logo">
+                <a href="#" class="js-logo-clone">MediTechBD</a>
+              </div>
+            </div>
+
+            <div class="text-right col-6 col-md-4 order-3 order-md-3">
+              <div class="site-top-icons">
+                <ul>
+                  <li><a href="#"><span class="icon glyphicon glyphicon-user"></span></a></li>
+                  <li><a href="#" class="site-cart"><span class="icon glyphicon glyphicon-shopping-cart"></span>
+                  <li><a href="#"><span class="glyphicon glyphicon-log-out"></span></a></li>
+                    </a>
+                  </li> 
+                </ul>
+              </div> 
+            </div>
+
+          </div>
+        </div>
+      </div> 
+      <nav class="site-navigation text-md-center" role="navigation">
+        <div class="container">
+          <ul class="site-menu">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About us</a></li>
+            <li><a href="shop.php">Shop</a></li>
+            <li class="active"><a href="gellary.php">Doctors</a></li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+    
+    
+    <div class="bg-light py-3">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 mb-0"><a href="index.php">Home</a> <span class="mx-2 mb-0">/</span> <strong class="text-black">About</strong></div>
+        </div>
+      </div>
+    </div> 
+
+    <div class="container">
+    <div class="row row-cols-1 row-cols-md-2 g-4">
+        <div class="col">
+          <div class="card border-dark mb-3">
+            <img height="300px" width="500px" src="images/doc-1.jpg" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Prof. Dr. Sangjukta Saha</h5>
+                <p class="card-text">Prof. Dr. Sangjukta Saha has 16 years of experience in the field of Laparoscopic Surgery.</p>
+                <a href="https://www.facebook.com/DrSangjuktaSaha">FaceBook</a>
+                <a href="https://www.youtube.com/channel/UC7qXNzglaQS71LlpnBN-lZQ">YouTube</a>  
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card border-dark mb-3">
+            <img height="300px" width="200px" src="images/doc-2.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Doctor Lutfor Rahman</h5>
+              <p class="card-text">Chief Cardiac Surgeon</p>
+              <a href="https://www.facebook.com/doctorlutforrahman">FaceBook</a>
+              <a href="https://www.youtube.com/channel/UCN1NKjTFD98v8xT6ZsHI7CA">YouTube</a> 
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card border-dark mb-3">
+            <img height="300px" width="200px" src="images/doc-3.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Professor Dr Altaf Sarker</h5>
+              <p class="card-text">উপমহাদেশের অন্যতম প্রখ্যাত ফিজিওথেরাপিস্ট এবং আন্তর্জাতিক খ্যাতিসম্পন্ন ব্যাকপেইন বিশেষজ্ঞ প্রফেসর ডা. আলতাফ হোসেন সরকার পাবনার অন্যতম কৃতীসন্তান ।</p>
+              <a href="https://www.facebook.com/ProfAltafSarker">FaceBook</a>
+              <a href="https://www.youtube.com/channel/UCRIbfD7_b2goiGmTIvW95rw">YouTube</a>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card border-dark mb-3">
+            <img height="300px" width="200px" src="images/doc-4.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Dr. Shusama Reza            </h5>
+              <p class="card-text">Dr. Shusama Reza
+                MBBS, M.D. (BSMMU) Lead Sexologist & Head of Sexual Medicine Unit, LifeSpring Limited.</p>
+              <a href="https://www.facebook.com/DrShusamaReza">FaceBook</a>
+              <a href="https://www.youtube.com/channel/UCIhnNMshTmGQmkJaBLGe5aw">YouTube</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row row-cols-1 row-cols-md-2 g-4">
+        <div class="col">
+            <div class="card border-dark mb-3">
+              <img height="300px" width="500px" src="images/doc-5.jpg" alt="...">
+              <div class="card-body">
+                  <h5 class="card-title">Dr Afrin Sultana                </h5>
+                  <p class="card-text">Dr Afrin Sultana is a promising qualified Female Surgeon in Bangladesh. She has been trained in best institutions (both national & international ) especially in Breast Surgery & colo-rectal Surgery.</p>
+                  <a href="https://www.facebook.com/doctor.afrin">FaceBook</a>
+                  <a href="https://www.youtube.com/channel/UCG64NWbdGX5Bl5rMLQ6UWJw">YouTube</a>  
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card border-dark mb-3">
+              <img height="300px" width="200px" src="images/doc-6.jpg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">Prof. Dr. Selina Akter                </h5>
+                <p class="card-text">Women's Health Clinic</p>
+                <a href="https://www.facebook.com/dr.selinaakter">FaceBook</a>
+                <a href="https://www.youtube.com/channel/UCSi8EG-Z52BLZ-BoCvMjRuA">YouTube</a> 
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card border-dark mb-3">
+              <img height="300px" width="200px" src="images/doc-7.jpg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">Prof. Dr. Munira Ferdausi                </h5>
+                <p class="card-text">She has over 22 years of experience in the field of Obstetrics & Gynecology. She is trained in Advanced Laparoscopic surgery also has developed her skills in Modern Infertility & IVF. Master trainer on Cervical Cancer, Breast Cancer Screening & Colposcopy                </p>
+                <a href="https://www.facebook.com/Prof.dr.MuniraFerdausi">FaceBook</a>
+                <a href="https://www.youtube.com/channel/UCED7xmabKKNABcymrFSnsnA">YouTube</a>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card border-dark mb-3">
+              <img height="300px" width="200px" src="images/doc-8.jpg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">Dr. Fariyal Hoque                </h5>
+                <p class="card-text">Dermatologist , Cutaneous and Aesthetic surgeon</p>
+                <a href="https://www.facebook.com/dr.fariyalhoque">FaceBook</a>
+                <a href="https://www.youtube.com/channel/UCy9ylsZzaF0at9HJFCs5HHw?view_as=subscriber&fbclid=IwAR0LN7At5byI39ZEslTsh4_qdgZEhhqHX9x6fsHH5jn-c3DHFVLfbAcpjbQ">YouTube</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row row-cols-1 row-cols-md-2 g-2">
+            <div class="col">
+                <div class="card border-dark mb-3">
+                  <img height="300px" width="500px" src="images/doc-9.jpg" alt="...">
+                  <div class="card-body">
+                      <h5 class="card-title">Dr. Ayesha Raisul                    </h5>
+                      <p class="card-text">Dr. Ayesha Raisul B.H.M.S (University Of Dhaka), Govt: Reg: H-1598 Ex- House Physician.Bangladesh Homeopathy Medical College & Hospital.</p>
+                      <a href="https://www.facebook.com/Dr.AyeshaRaisul.B.H.M.S">FaceBook</a>
+                      <!-- <a href="https://www.youtube.com/channel/UCG64NWbdGX5Bl5rMLQ6UWJw">YouTube</a>   -->
+                  </div>
+                </div>
+              </div>
+              <div class="col">
+                <div class="card border-dark mb-3">
+                  <img height="300px" width="200px" src="images/doc-10.png" class="card-img-top" alt="...">
+                  <div class="card-body">
+                    <h5 class="card-title">Dr.Md.Shafiullah Prodhan                    </h5>
+                    <p class="card-text">বাত ব্যাথা, প্যারালাইসিস, হাড় জোড়া রোগ ও রিহেব-ফিজিও বিশেষজ্ঞ।</p>
+                    <a href="https://www.facebook.com/DrShafiullahProdhan">FaceBook</a>
+                    <a href="https://www.youtube.com/c/DrMdShafiullahProdhan">YouTube</a> 
+                  </div>
+                </div>
+              </div>
+            </div>  
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+  </body>
+</html>
